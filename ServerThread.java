18c222a5603b20348c6a72d1660d9696a359e1f2
Serge009 <serge.naumovych@gmail.com>
2014-03-20 20:55:51 +0200

    Create ServerThread.java

---
+++ ServerThread.java
@@ -0,0 +1,46 @@
+package Socket;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+/**
+ * Created by 23 on 20.03.14.
+ */
+public class ServerThread implements Runnable {
+    private Socket socket;
+
+    public ServerThread(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Got a client :) ... Finally, someone saw me through all the cover!");
+        System.out.println();
+        try {
+            // Берем входной и выходной потоки сокета, теперь можем получать и отсылать данные клиенту.
+            InputStream sin = socket.getInputStream();
+            OutputStream sout = socket.getOutputStream();
+
+            // Конвертируем потоки в другой тип, чтоб легче обрабатывать текстовые сообщения.
+            DataInputStream in = new DataInputStream(sin);
+            DataOutputStream out = new DataOutputStream(sout);
+
+            String line = null;
+            while(true) {
+                line = in.readUTF(); // ожидаем пока клиент пришлет строку текста.
+                System.out.println("The dumb client just sent me this line : " + line);
+                System.out.println("I'm sending it back...");
+                out.writeUTF(line); // отсылаем клиенту обратно ту самую строку текста.
+                out.flush(); // заставляем поток закончить передачу данных.
+                System.out.println("Waiting for the next line...");
+                System.out.println();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
